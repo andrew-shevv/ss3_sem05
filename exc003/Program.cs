b0ca0a131cce3edcac590938d70efc393c74bbcc
Andrew <vishnyaishere@gmail.com>
2022-08-25 23:01:14 +0300

    exc003 done

--- exc003/Program.cs
+++ exc003/Program.cs
@@ -3,3 +3,27 @@
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, 3] -> да
 
+int[] array = new int[5];
+
+for (int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(-999, 1000);
+}
+
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+Console.Write( "Input number: ");
+int fnd = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Is {fnd} part of this array?");
+bool check = false;
+for (int i = 0; i < array.Length; i++){
+    if (array[i] == fnd){
+        check = true;
+        Console.Write("yes");
+        break; 
+    }
+}
+
+if (check == false){
+    Console.WriteLine("no");
+}
