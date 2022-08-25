de0ecc1c7a737df7ef65e948680303ae0fccf569
Andrew <vishnyaishere@gmail.com>
2022-08-25 22:14:47 +0300

    exc001 done

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -4,3 +4,25 @@
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
 // сумма положительных чисел равна 29, сумма отрицательных равна -20.
 
+int[] array = new int[12];
+
+for (int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(-9, 10);
+}
+
+int sumPos = 0;
+int sumNeg = 0;
+for (int i = 0; i < array.Length; i++){
+    if (array[i] > 0){
+        sumPos += array[i];
+    }
+    else if (array[i] < 0){
+        sumNeg += array[i];
+    }
+}
+
+Console.WriteLine(
+    '[' + string.Join(", ", array) + ']'
+    + $"\nsum of positive numbers = {sumPos}" 
+    + $"\nsum of negative numbers = {sumNeg}"
+);
