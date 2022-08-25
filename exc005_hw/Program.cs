9fc8186f943df4de8e3bd4420f3d68ba313cd8f5
Andrew <vishnyaishere@gmail.com>
2022-08-25 23:34:18 +0300

    exc005 done

--- exc005_hw/Program.cs
+++ exc005_hw/Program.cs
@@ -2,3 +2,15 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+int[] array = new int[5];
+
+int count = 0;
+for (int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0){
+        count++;
+    }
+}
+
+Console.Write('[' + string.Join(", ", array) + ']' + " ==> ");
+Console.Write($"{count} even numbers in the array");
