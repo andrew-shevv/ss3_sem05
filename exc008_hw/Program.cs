b02058472f03fdc7b810d0182a0489fa5133a6b1
Andrew <vishnyaishere@gmail.com>
2022-08-26 00:28:16 +0300

    exc008 done

--- exc008_hw/Program.cs
+++ exc008_hw/Program.cs
@@ -2,3 +2,25 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
+double[] array = new double[new Random().Next(3, 10)];
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++){
+    array[i] = rand.Next(1, 100);
+}
+
+Console.Write('[' + string.Join(", ", array) + ']' + " ==> ");
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++){
+    if (array[i] < min){
+        min = array[i];
+    }
+    else if (array[i] > max){
+        max = array[i];
+    }
+}
+
+Console.WriteLine($"{max - min} is the diff between highest and lowest value in the array");
