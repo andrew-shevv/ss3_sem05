f2bdffc6228115d2806baf47f412b74c8e4d2782
Andrew <vishnyaishere@gmail.com>
2022-08-26 00:41:50 +0300

    exc008 minor optimisation

--- exc008_hw/Program.cs
+++ exc008_hw/Program.cs
@@ -14,7 +14,7 @@ Console.Write('[' + string.Join(", ", array) + ']' + " ==> ");
 double min = array[0];
 double max = array[0];
 
-for (int i = 0; i < array.Length; i++){
+for (int i = 1; i < array.Length; i++){
     if (array[i] < min){
         min = array[i];
     }
